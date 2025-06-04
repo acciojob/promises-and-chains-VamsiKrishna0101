4c973d1d4e2c88c84f298957c99934f0877fecaf
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-04 18:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function validate() {
+	const age=document.getElementById("age").value
+	const name=document.getElementById("name").value
+	if(age<0||!name.trim()){
+		alert("Please enter valid details")
+		return
+	}
+	let mypromise=new Promise((res,rej)=>{
+         setTimeout(()=>{
+			 if(age<18){
+				 rej(`oh sorry ${name}. You aren't old enough.`)
+			 }else{
+				 res(`Welcome, ${name}. You can vote.`)
+			 }
+		 },4000)
+	})
+	mypromise.then((message)=>alert(message))
+	.catch((message)=>alert(message))
+}
